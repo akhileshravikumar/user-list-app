82187d286c113d529ff4f4c4fff5100be17b7ce2
akhileshravikumar <akhi2431@gmail.com>
2023-09-06 11:07:09 +0530

    minor bug fix.

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,22 +2,22 @@ import React from "react";
 import Button from "../../Utils/Button";
 import './Error.css'
 
-const Error = ({setFlag})=>{
-    return(<div className="backdrop">
-         <div className="modal">
+const Error = ({ setFlag }) => {
+    return (<div className="backdrop">
+        <div className="modal">
             <header className="header" >
                 <h2>Error</h2>
             </header>
             <div className="content">
-                <p>Invlid input given. Please Check</p>
-                <div className="actions" onClick={()=>{setFlag(false)}} >
-                    <Button value="Okay"/>
+                <p>Invalid input given. Please Check</p>
+                <div className="actions" onClick={() => { setFlag(false) }} >
+                    <Button value="Okay" />
                 </div>
             </div>
-           
-         </div>
 
-        </div>)
+        </div>
+
+    </div>)
 }
 
 export default Error;
